af4da2cec8cbb9f5551ec9a2c8c586c8da8cf77c
David Radice <dradice@caltech.edu>
2015-12-05 11:13:57 -0800

    OS-X Cactus config: do not use OpenMP if not requested

--- templates/cactus/configs/osx-macports-gcc.mk
+++ templates/cactus/configs/osx-macports-gcc.mk
@@ -1,4 +1,4 @@
-VERSION = 2015-07-19
+VERSION = 2015-11-19
 
 CPP = /opt/local/bin/cpp
 FPP = /opt/local/bin/cpp
@@ -7,12 +7,12 @@ CXX = /opt/local/bin/g++
 F77 = /opt/local/bin/gfortran
 F90 = /opt/local/bin/gfortran
 
-CPPFLAGS = -fopenmp 
-FPPFLAGS = -fopenmp -traditional
-CFLAGS   = -fopenmp -std=c99
-CXXFLAGS = -fopenmp 
-F77FLAGS = -fopenmp -m128bit-long-double -fcray-pointer -ffixed-line-length-none -fno-range-check
-F90FLAGS = -fopenmp -m128bit-long-double -fcray-pointer -ffixed-line-length-none -fno-range-check
+CPPFLAGS =
+FPPFLAGS = -traditional
+CFLAGS   = -std=c99
+CXXFLAGS =
+F77FLAGS = -m128bit-long-double -fcray-pointer -ffixed-line-length-none -fno-range-check
+F90FLAGS = -m128bit-long-double -fcray-pointer -ffixed-line-length-none -fno-range-check
 
 LDFLAGS = -framework Accelerate -lgfortran
 
@@ -47,6 +47,14 @@ C_PROFILE_FLAGS   =
 CXX_PROFILE_FLAGS =
 F77_PROFILE_FLAGS =
 F90_PROFILE_FLAGS =
+
+OPENMP           = yes
+CPP_OPENMP_FLAGS = -openmp
+FPP_OPENMP_FLAGS = -D_OPENMP
+C_OPENMP_FLAGS   = -openmp
+CXX_OPENMP_FLAGS = -openmp
+F77_OPENMP_FLAGS = -openmp
+F90_OPENMP_FLAGS = -openmp
 
 WARN           = yes
 CPP_WARN_FLAGS = -Wall -Wno-unknown-pragmas
