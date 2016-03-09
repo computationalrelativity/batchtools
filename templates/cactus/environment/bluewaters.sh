4162865d7fd9861d1761d1621d92732e8932c96e
David Radice <dradice@caltech.edu>
2016-03-09 10:49:28 -0800

    Added templates for Cactus/BlueWaters

---
+++ templates/cactus/environment/bluewaters.sh
@@ -0,0 +1,14 @@
+source /etc/profile
+module unload PrgEnv-cray PrgEnv-gnu PrgEnv-intel PrgEnv-pathscale PrgEnv-pgi
+module load PrgEnv-gnu
+module load acml/5.3.1
+module load atp
+module load boost/1.53.0
+module load cray-hdf5/1.8.13
+module load cray-petsc/3.6.1.0
+module load cudatoolkit/7.0.28-1.0502.10742.5.1
+module load fftw/3.3.4.6
+module load gsl/1.15.1
+#module load papi/5.4.1.3
+#module load perftools/6.2.1
+module load pmi
