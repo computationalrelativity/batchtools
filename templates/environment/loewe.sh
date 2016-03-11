543c4beedd881ba83490a533fba1a51895d362cb
David Radice <dradice@caltech.edu>
2016-03-11 14:16:29 -0800

    Added loewe environment, fixed bluewaters batchscript.

---
+++ templates/environment/loewe.sh
@@ -0,0 +1,7 @@
+source /etc/profile
+module purge
+module load unstable
+module load slurm/2.6.3
+module load intel/compiler/64/15.0.0
+module load mpi/mvapich2/intel-15.0.0/2.0
+module load hdf5/intel-15.0.0/1.8.14
