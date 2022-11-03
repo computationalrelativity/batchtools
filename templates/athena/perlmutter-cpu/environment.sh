58d37be98b8f10129724282b0db67889f05b3591
Francesco Zappa <francesco.zappa@uni-jena.de>
2022-11-03 09:44:05 -0700

    Add correct environment for perlmutter, using cray

--- templates/athena/perlmutter-cpu/environment.sh
+++ templates/athena/perlmutter-cpu/environment.sh
@@ -1,9 +1,5 @@
 module purge
 
-module load PrgEnv-gnu
-module load craype-x86-milan
-module use /global/common/software/m3169/perlmutter/modulefiles
-module load openmpi
-module load python
-module load cray-hdf5-parallel
+module load PrgEnv-cray
 module load gsl
+module load cray-hdf5-parallel
