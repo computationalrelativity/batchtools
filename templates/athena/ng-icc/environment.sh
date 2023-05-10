f1f203603d15d60391664cfdfae5b19024e527a6
ku69yet <francesco.zappa@uni-jena.de>
2023-05-10 17:36:36 +0200

    Add batch scripts, test parfile, environment for LRZ ng for athena compiled with gcc and intel

---
+++ templates/athena/ng-icc/environment.sh
@@ -0,0 +1,7 @@
+module purge
+module use /dss/dsshome1/0D/di52gal3/spack/modules/x86_avx512/linux-sles15-skylake_avx512/
+module load spack/22.2.1
+module load intel-oneapi-compilers/2021.4.0
+module load intel-oneapi-mpi/2021.7.0-intel
+module load gsl hdf5/1.10.7-intel21-impi
+module load anaconda3/2021.05
