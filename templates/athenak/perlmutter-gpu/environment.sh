88b9d4cdad2ad869d892a83f246cd13e1ab2ace9
Jacob Fields <jmf6719@psu.edu>
2023-07-06 08:05:00 -0700

    Updated AthenaK Perlmutter configuration. Need to update submission script.

--- templates/athenak/perlmutter-gpu/environment.sh
+++ templates/athenak/perlmutter-gpu/environment.sh
@@ -1,3 +1,8 @@
 module purge
 module load PrgEnv-gnu
-module load cudatoolkit/11.4
+module load cudatoolkit/11.7
+module load craype-accel-nvidia80
+module load cpe/23.03
+module load craype-x86-milan
+module load xpmem
+module load gpu/1.0
