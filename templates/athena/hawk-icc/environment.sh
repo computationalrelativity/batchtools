63f7d568e1284cc04a85e0c96b508292a577975a
ku69yet <francesco.zappa@uni-jena.de>
2023-08-09 15:01:39 +0200

    Add modules to be loaded in hawk envs for athena

--- templates/athena/hawk-icc/environment.sh
+++ templates/athena/hawk-icc/environment.sh
@@ -1,4 +1,5 @@
 module purge
+module load system/site_names system/ws/1.4.0 system/wrappers/1.0
 module load hlrs-software-stack/previous hlrs-software-stack/.4  intel/19.1.0  mpt/2.23
 module load hdf5/1.10.5
 module load gsl
