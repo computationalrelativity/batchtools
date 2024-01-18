7e105dd190e5a357b3d47e22e070bf773495992d
Jacob Fields <jmf6719@psu.edu>
2024-01-18 06:59:01 -0800

    Updated AthenaK Perlmutter GPU environment.

--- templates/athenak/perlmutter-gpu/environment.sh
+++ templates/athenak/perlmutter-gpu/environment.sh
@@ -1,8 +1,8 @@
 module purge
+module load cpe/23.12
 module load PrgEnv-gnu
-module load cudatoolkit/11.7
+module load cudatoolkit/11.2
 module load craype-accel-nvidia80
-module load cpe/23.03
 module load craype-x86-milan
 module load xpmem
 module load gpu/1.0
