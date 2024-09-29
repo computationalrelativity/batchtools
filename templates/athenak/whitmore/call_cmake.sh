f26186587b1b86306b3578b82beabc974959ecfb
David Radice <david.radice@psu.edu>
2024-09-29 12:06:55 -0400

    Fix cmake call for Whitmore

--- templates/athenak/whitmore/call_cmake.sh
+++ templates/athenak/whitmore/call_cmake.sh
@@ -1,6 +1,6 @@
 export CUDA_ROOT=/usr/local/cuda-12.6
-echo cmake \
+cmake \
     -DKokkos_ENABLE_CUDA=ON \
     -DKokkos_ARCH_PASCAL61=ON \
     -DCMAKE_CXX_COMPILER=$PWD/../kokkos/bin/nvcc_wrapper \
-    $* ..
+    $@ ..
