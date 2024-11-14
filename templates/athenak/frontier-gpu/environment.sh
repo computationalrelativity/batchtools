b9f0940fe667b5f30f95eaa0d4ff54398154f67e
Jacob Fields <jfields7@login13.frontier.olcf.ornl.gov>
2024-11-14 14:02:00 -0500

    Updated AthenaK Frontier modules.

--- templates/athenak/frontier-gpu/environment.sh
+++ templates/athenak/frontier-gpu/environment.sh
@@ -3,8 +3,8 @@ module load PrgEnv-cray
 module load craype-accel-amd-gfx90a
 module load cmake
 module load cray-python
-module load amd-mixed/5.3.0
-module load cray-mpich/8.1.23
-module load cce/15.0.1
+module load rocm/5.7.1
+module load cray-mpich/8.1.28
+module load cce/17.0.0
 
 export MPICH_GPU_SUPPORT_ENABLED=1
