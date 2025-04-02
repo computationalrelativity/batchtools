40cf2a673e6c4f6ad3646fa70167cb6e32b8497d
David Radice <dur566@psu.edu>
2025-04-02 12:50:14 -0400

    Add GSL_ROOD_DIR in Aurora config

--- templates/athenak/aurora/configure.sh
+++ templates/athenak/aurora/configure.sh
@@ -1,5 +1,6 @@
 cmake   -DAthena_ENABLE_MPI=ON \
 	-DCMAKE_CXX_COMPILER=icpx \
+	-DGSL_ROOT_DIR=`gsl-config --prefix` \
 	-DKokkos_ENABLE_SERIAL=ON \
 	-DKokkos_ENABLE_SYCL=ON \
 	-DKokkos_ENABLE_SYCL_RELOCATABLE_DEVICE_CODE=ON \
