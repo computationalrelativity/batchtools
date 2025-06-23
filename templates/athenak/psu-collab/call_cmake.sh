d91f7c3c67de32b54727c14f41f41b17c68b6e8a
David Radice <dur566@psu.edu>
2025-06-23 15:20:12 -0400

    Update setup for Roar Collab

--- templates/athenak/psu-collab/call_cmake.sh
+++ templates/athenak/psu-collab/call_cmake.sh
@@ -1,5 +1,6 @@
 cmake \
     -DKokkos_ENABLE_CUDA=ON \
     -DKokkos_ARCH_AMPERE80=ON \
+    -DKokkos_ENABLE_IMPL_CUDA_MALLOC_ASYNC=OFF \
     -DCMAKE_CXX_COMPILER=$PWD/../kokkos/bin/nvcc_wrapper \
     $@ ..
