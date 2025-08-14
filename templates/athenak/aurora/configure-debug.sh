83ecfa0c6c2ac28a23c363e15405237c808b1f0f
David Radice <dur566@psu.edu>
2025-08-14 17:09:30 -0400

    Remove KOKKOS_DEBUG from AthenaK/Aurora debug setup

--- templates/athenak/aurora/configure-debug.sh
+++ templates/athenak/aurora/configure-debug.sh
@@ -1,8 +1,6 @@
 cmake   -DAthena_ENABLE_MPI=ON \
 	-DCMAKE_EXE_LINKER_FLAGS='-fsycl-max-parallel-link-jobs=16 -fsycl-device-code-split=per_kernel' \
 	-DCMAKE_BUILD_TYPE='Debug' \
-	-DKokkos_ENABLE_DEBUG=ON \
-	-DKokkos_ENABLE_DEBUG_BOUNDS_CHECK=ON \
 	-DCMAKE_CXX_COMPILER=icpx \
 	-DGSL_ROOT_DIR=`gsl-config --prefix` \
 	-DKokkos_ENABLE_SERIAL=ON \
