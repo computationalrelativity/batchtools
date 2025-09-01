cf9ca8cb756e987958e30630c5f37ae3a0346728
Federico Maria Guercilena <federico.guercilena@unitn.it>
2025-09-01 16:02:32 +0200

    Work on AthenaK template for Leoinado-booster at CINECA

---
+++ templates/athenak/leonardo-booster/config.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+cmake -DAthena_ENABLE_MPI=ON \
+      -DKokkos_ENABLE_CUDA=ON \
+      -DKokkos_ENABLE_CUDA_LAMBDA=ON \
+      -DKokkos_ENABLE_IMPL_CUDA_MALLOC_ASYNC=OFF \
+      -DKokkos_ARCH_AMPERE80=On \
+      -DKokkos_ARCH_ICX=On \
+      $@
+      -Bbuild
---
+++ templates/athenak/leonardo-booster/environment.sh
@@ -0,0 +1,7 @@
+module load --silent gcc/12.2.0
+module load --silent cuda/12.3
+module load --silent openmpi/4.1.6--gcc--12.2.0
+module load --silent python/3.11.6--gcc--8.5.0
+module load --silent openblas/0.3.24--gcc--12.2.0
+module load --silent gsl/2.7.1--gcc--12.2.0
+module load --silent fftw/3.3.10--openmpi--4.1.6--gcc--12.2.0
