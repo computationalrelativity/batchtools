f3d466b58f2932ca58829ecae0161863329b73bd
David Radice <dur566@psu.edu>
2026-05-21 11:27:30 -0400

    Update athenak/whitmore setup

--- templates/athenak/whitmore/call_cmake.sh
+++ templates/athenak/whitmore/call_cmake.sh
@@ -1,5 +1,6 @@
 cmake \
     -DKokkos_ENABLE_CUDA=ON \
     -DKokkos_ARCH_PASCAL61=ON \
+    -DCMAKE_CXX_FLAGS='-Wno-deprecated-gpu-targets' \
     -DCMAKE_CXX_COMPILER=$PWD/../kokkos/bin/nvcc_wrapper \
     $@ ..
